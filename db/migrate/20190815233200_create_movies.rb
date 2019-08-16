53962cf546d2574344c68b9f800cea7cfa9f6c9e
andrewrosario <rosario.andrew@gmail.com>
2019-08-15 19:34:30 -0500

    Done.

---
+++ db/migrate/20190815233200_create_movies.rb
@@ -0,0 +1,10 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |m|
+      m.string :title
+      m.integer :release_date
+      m.string :lead
+      m.boolean :in_theaters
+    end
+  end
+end
---
+++ db/migrate/20190815233800_add_directors.rb
@@ -0,0 +1,5 @@
+class AddDirectors < ActiveRecord::Migration[5.2]
+  def change
+    add_column :movies, :director, :string
+  end
+end
